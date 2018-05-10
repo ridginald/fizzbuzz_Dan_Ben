a2bb22887f367b7536fc130c5c5453378d0b7590
Daniel Ridgway <ridginald@gmail.com>
2018-05-10 15:19:36 +0100

    28

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when passed 28' do
+    expect(fizzbuzz(28)).to eq 'fizzbuzz'
 end
