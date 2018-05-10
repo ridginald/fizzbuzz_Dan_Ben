b1679720570311c9d79c617b6dffcc6ed628082e
Benjamin Tomkins <bentomkins@hotmail.com>
2018-05-10 15:39:22 +0100

    solution to test4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 30' do
     expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end
+
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
